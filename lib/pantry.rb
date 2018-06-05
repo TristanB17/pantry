51eb3e43c73f4bd4c98dbef0f076aa6719925671
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-06-05 10:35:18 -0600

    Reworking code for iteration 3

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,17 +1,17 @@
 class Pantry
   attr_reader       :stock,
-                    :shopping_list
+                    :shopping_list,
+                    :cookbook
 
   def initialize
     @stock = Hash.new(0)
     @shopping_list = Hash.new(0)
+    @cookbook = []
   end
 
   def stock_check(item)
     if @stock[item] != nil
-      return @stock[item]
-    else
-      0
+        @stock[item]
     end
   end
 
@@ -19,8 +19,8 @@ class Pantry
     @stock[item] += quantity
   end
 
-  def add_to_shopping_list(ingredients)
-    ingredients.each_pair do |key, value|
+  def add_to_shopping_list(recipe)
+    recipe.ingredients.each_pair do |key, value|
       @shopping_list[key] += value
     end
   end
@@ -31,5 +31,34 @@ class Pantry
       list << "* #{key}: #{value}"
     end
     list.join("\n")
+  end
+
+  def add_to_cookbook(recipe)
+    @cookbook << recipe
+  end
+
+  def what_can_i_make
+    binding.pry
+    to_make = []
+    @cookbook.each do |recipe|
+      if check_ingredients(recipe)
+        to_make << recipe.name
+      end
+    end
+  end
+
+  def check_ingredients(recipe)
+    binding.pry
+    enough = []
+    recipe.ingredients.each_pair do |key, value|
+      if stock_check(key) > value
+        enough << key
+      end
+    end
+    binding.pry
+    if recipe.ingredients.values.count == enough.count
+      true
+    end
+    binding.pry
   end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -40,7 +40,7 @@ class PantryTest < Minitest::Test
 
     recipe.add_ingredient("Cheese", 20)
     recipe.add_ingredient("Flour", 20)
-    @pantry.add_to_shopping_list(recipe.ingredients)
+    @pantry.add_to_shopping_list(recipe)
 
     assert_equal({"Cheese" => 20, "Flour" => 20}, @pantry.shopping_list)
   end
@@ -50,14 +50,14 @@ class PantryTest < Minitest::Test
 
     recipe_1.add_ingredient("Cheese", 20)
     recipe_1.add_ingredient("Flour", 20)
-    @pantry.add_to_shopping_list(recipe_1.ingredients)
+    @pantry.add_to_shopping_list(recipe_1)
 
     recipe_2 = Recipe.new("Spaghetti")
 
     recipe_2.add_ingredient("Spaghetti Noodles", 10)
     recipe_2.add_ingredient("Marinara Sauce", 10)
     recipe_2.add_ingredient("Cheese", 5)
-    @pantry.add_to_shopping_list(recipe_2.ingredients)
+    @pantry.add_to_shopping_list(recipe_2)
 
     assert_equal({
       "Cheese" => 25,
@@ -72,16 +72,65 @@ class PantryTest < Minitest::Test
 
     recipe_1.add_ingredient("Cheese", 20)
     recipe_1.add_ingredient("Flour", 20)
-    @pantry.add_to_shopping_list(recipe_1.ingredients)
+    @pantry.add_to_shopping_list(recipe_1)
 
     recipe_2 = Recipe.new("Spaghetti")
 
     recipe_2.add_ingredient("Spaghetti Noodles", 10)
     recipe_2.add_ingredient("Marinara Sauce", 10)
     recipe_2.add_ingredient("Cheese", 5)
-    @pantry.add_to_shopping_list(recipe_2.ingredients)
+    @pantry.add_to_shopping_list(recipe_2)
 
     assert_equal "* Cheese: 25\n* Flour: 20\n* Spaghetti Noodles: 10\n* Marinara Sauce: 10", @pantry.print_shopping_list
+  end
+
+  def test_pantry_has_cookbook
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    @pantry.add_to_cookbook(r1)
+    @pantry.add_to_cookbook(r2)
+    @pantry.add_to_cookbook(r3)
+
+    assert_equal 3, @pantry.cookbook.length
+    assert_equal r3, @pantry.cookbook[2]
+  end
+
+  def test_pantry_can_determine_what_can_be_made_based_on_stock
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    @pantry.add_to_cookbook(r1)
+    @pantry.add_to_cookbook(r2)
+    @pantry.add_to_cookbook(r3)
+
+    @pantry.restock("Cheese", 10)
+    @pantry.restock("Flour", 20)
+    @pantry.restock("Brine", 40)
+    @pantry.restock("Cucumbers", 120)
+    @pantry.restock("Raw nuts", 20)
+    @pantry.restock("Salt", 20)
+
+    binding.pry
+    assert_equal ["Pickles", "Peanuts"], @pantry.what_can_i_make
   end
 
 end
